95b3a21a9fe029aa6b908d86ca0c5abfdd291474
benjaminrall <benr2508@aol.com>
2024-12-16 18:35:04 +0000

    Day 16

--- day-16/part1.py
+++ day-16/part1.py
@@ -1,13 +1,10 @@
 # Useful imports
-import pyaoc
 import math
 import numpy as np
 from functools import cache
 from collections import defaultdict
 import heapq
 
-import sys
-sys.setrecursionlimit(100000)
 
 # Placeholders to be filled when copying the template
 PART = 1
@@ -19,40 +16,7 @@ TEST_RESULT = 7036
 
 DIRS = [-1, 1j, 1, -1j]
 
-def get_neighbours(current):
-    cdir = DIRS[current[1]]
-    for i, dir in enumerate([cdir * 1j, cdir, cdir * -1j]):
-        i = (current[1] - 1 + i) % 4
-        cost = 1 if current[1] == i else 1001
-        yield (current[0] + dir, i), cost
-
-def dijkstra(graph: list[list[int]], weight: list[list[float]], source: int = 0, target: int | None = None) -> tuple[list[float], list[int]]:
-    # Graph is an adjacency list, weight is a weight matrix
-    n = len(graph)
-    prec = defaultdict(lambda : None)
-    black = defaultdict(lambda : False)
-    dist = defaultdict(lambda : float('inf'))
-    dist[source] = 0
-    heap = [(0, source[0].real, source[0].imag, source)]
-    while heap:
-        dist_node, _, _ , node= heapq.heappop(heap) # Closest node from source
-        if not black[node]:
-            black[node] = True
-            if node[0] == target:
-                return dist_node
-            for neighbor in graph[node]:
-                dist_neighbor = dist_node + weight[(node, neighbor)]
-                print(node, neighbor, weight[(node, neighbor)], dist_node, dist_neighbor)
-                if dist_neighbor < dist[neighbor]:
-                    dist[neighbor] = dist_neighbor
-                    prec[neighbor] = node
-                    
-                    heapq.heappush(heap, (dist_neighbor, neighbor[0].real, neighbor[0].imag, neighbor))
-    return dist, prec        
-
-
 def construct(neighbours, dists, grid, visited, current):
-    
     visited.add(current)
     # print(f"\nCURRENT: {current}")
     for neighbour, dist in get_neighbours(current):
@@ -64,8 +28,38 @@ def construct(neighbours, dists, grid, visited, current):
         dists[(neighbour, current)] = dist
         construct(neighbours, dists, grid, visited, neighbour)
 
-def to_tup(n):
-    return (n.real, n.imag)
+def get_neighbours(grid, current):
+    cdir = DIRS[current[1]]
+    for i, dir in enumerate([cdir * 1j, cdir, cdir * -1j]):
+        i = (current[1] - 1 + i) % 4
+        cost = 1 if current[1] == i else 1001
+        neighbour = current[0] + dir
+        if neighbour in grid:
+            yield (neighbour, i), cost
+
+def dijkstra(grid, source: int = 0, target: int | None = None) -> tuple[list[float], list[int]]:
+    # Graph is an adjacency list, weight is a weight matrix
+    prec = defaultdict(lambda : None)
+    black = defaultdict(lambda : False)
+    dist = defaultdict(lambda : float('inf'))
+    dist[source] = 0
+    heap = [(0, source[0].real, source[0].imag, source)]
+    while heap:
+        dist_node, _, _ , node= heapq.heappop(heap) # Closest node from source
+        if not black[node]:
+            black[node] = True
+            if node[0] == target:
+                return dist_node
+            for neighbor, w in get_neighbours(grid, node):
+                dist_neighbor = dist_node + w
+                # print(node, neighbor, weight[(node, neighbor)], dist_node, dist_neighbor)
+                if dist_neighbor < dist[neighbor]:
+                    dist[neighbor] = dist_neighbor
+                    prec[neighbor] = node
+                    
+                    heapq.heappush(heap, (dist_neighbor, neighbor[0].real, neighbor[0].imag, neighbor))
+    return dist, prec        
+
 
 # Method to solve the input stored in a given file name
 def solve(filename: str) -> int:
@@ -88,13 +82,10 @@ def solve(filename: str) -> int:
     neighbours = defaultdict(set)
     dists = {}
     start = (start, 1)
-    construct(neighbours, dists, grid, set(), start)
+    # construct(neighbours, dists, grid, set(), start)
 
-    return dijkstra(neighbours, dists, start, end)
+    return dijkstra(grid, start, end)
 
-# Attempt to submit the current solve method
-pyaoc.submit(
-    solve, PART, DAY, YEAR, 
-    test_result=TEST_RESULT,
-    test=True
-)
+if __name__ == "__main__":
+    print(f"Test solution: {solve('test.txt')}")
+    print(f"Actual solution: {solve('input.txt')}")
